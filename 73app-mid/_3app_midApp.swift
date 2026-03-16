a881a30d1d63315c3ce4de4dfecc6789ec104465
Ray Hsu (MacBook Air M3) <s111319022@stu.ntue.edu.tw>
2026-03-16 10:44:26 +0800

    Initial Commit

---
+++ 73app-mid/_3app_midApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3app_midApp.swift
+//  73app-mid
+//
+//  Created by Ray Hsu on 2026/3/16.
+//
+
+import SwiftUI
+
+@main
+struct _3app_midApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
